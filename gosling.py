45f90c065573f5e8efa1355adaca8acb86efa479
ArtDoctor <artdoctorka@gmail.com>
2024-01-23 21:04:59 +0000

    Add image files and update file paths in gosling.py

--- gosling.py
+++ gosling.py
@@ -13,18 +13,18 @@ with TelegramClient('session_name', api_id, api_hash) as client:
         if  'Блин' == message[:4]:
             if random.random() < 0.1:
                 print('sent blin')
-                await event.respond(file='blin.jpg')
+                await event.respond(file='files/blin.jpg')
             else:
                 print('sent gosling')
-                await event.respond(file='400.gif')
+                await event.respond(file='files/400.gif')
         elif 'блин' == message[:4].lower():
             if random.random() < 0.1:
                 print('sent blin')
-                await event.respond(file='blin.jpg')
+                await event.respond(file='files/blin.jpg')
             else:
                 print('sent gosling')
-                await event.respond(file='300.gif')
+                await event.respond(file='files/300.gif')
         elif 'капец' == message[:5].lower():
-            await event.respond(file='kapets.gif')
+            await event.respond(file='files/kapets.gif')
 
     client.run_until_disconnected()
